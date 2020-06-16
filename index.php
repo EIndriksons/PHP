e98c446116a5c763c4d8945cf0b7d8440b7023c1
EIndriksons <edgars@indriksons.lv>
2020-06-16 22:26:25 +0300

    GET request

--- index.php
+++ index.php
@@ -1,15 +1,30 @@
-<?php
+<!DOCTYPE html>
+<html lang="en">
+<head>
+    <meta charset="UTF-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <title>PHP</title>
 
-$name = 'Jake';
+    <style>
+        header {
+            background: #e3e3e3;
+            padding: 2em;
+            text-align: center;
+        }
+    </style>
 
-// if you use ' then you cannot output variable directly
-// echo 'Hello, $greetings';
-
-// but if you use " you can do it like this:
-echo "Hello, $name";
-
-// another way, but mouch more clearer
-echo "Hello, {$name}";
-
-// another way
-echo 'Hello, ' . $name;
+</head>
+<body>
+    <header>
+        <h1>
+            <?php
+                // standard way
+                $name = $_GET['name'];
+                echo "Hello $name!";
+            ?>
+        </h1>
+    </header>
+    <h1><?php echo "Hello " . $_GET['name']; // another way ?></h1>
+    <h1><?= "Hello " . $_GET['name']; // shorter way ?></h1>
+</body>
+</html>
