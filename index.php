627314c2ca40335bc6ead315978970b4bb405e8e
EIndriksons <edgars@indriksons.lv>
2020-06-16 22:32:46 +0300

    PHP templates

--- index.php
+++ index.php
@@ -1,32 +1,6 @@
-<!DOCTYPE html>
-<html lang="en">
-<head>
-    <meta charset="UTF-8">
-    <meta name="viewport" content="width=device-width, initial-scale=1.0">
-    <title>PHP</title>
+<?php
 
-    <style>
-        header {
-            background: #e3e3e3;
-            padding: 2em;
-            text-align: center;
-        }
-    </style>
+$greeting = 'Hello, World!';
 
-</head>
-<body>
-    <header>
-        <h1>
-            <?php
-                // standard way
-                $name = htmlspecialchars($_GET['name']);
-                echo "Hello $name!";
-
-                // use htmlspecialchars() to prevent script injection
-            ?>
-        </h1>
-    </header>
-    <h1><?php echo "Hello " . htmlspecialchars($_GET['name']); // another way ?></h1>
-    <h1><?= "Hello " . htmlspecialchars($_GET['name']); // shorter way ?></h1>
-</body>
-</html>
+// basically 'import' the actual template page
+require 'index.view.php';
